596ddd63508dcdf548e6a6d8cad4dbdd1f8ac38b
Ryan Schneider <rslego@gmail.com>
2014-07-18 22:50:33 -0400

    Added basic aircraft objects

---
+++ Open Skies/Open Skies/Source/Aircraft.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Skies.Source {
+	class Aircraft {
+		// Public Independant Properties
+		public string name { get; private set; }
+		public PartNode basePart { get; private set; }
+
+		// Public Dependant Properties
+		public List<PartNode> partList {
+			get {
+				List<PartNode> list = new List<PartNode>();
+				List<PartNode> remaining = new List<PartNode>();
+				remaining.Add(basePart);
+
+				while (remaining.Count > 0) {
+					PartNode node = remaining.ElementAt(0);
+					list.Add(node);
+					remaining.Remove(node);
+
+					foreach (PartNode i in node.connectedNodes) {
+						if (!remaining.Contains(i)) remaining.Add(i);
+					}
+				}
+
+				return list;
+			}
+		}
+
+
+		// Constructors
+		public Aircraft(string name, PartNode basePart) {
+			this.name = name;
+			this.basePart = basePart;
+		}
+
+
+		// Methods
+		public List<Round> Fire(int delta) {
+			List<Round> rounds = new List<Round>();
+			
+			foreach (PartNode node in partList) {
+				if (node.part is Gun) {
+					List<Round> round = ((Gun)node.part).Shoot(delta);
+					if (round != null) rounds.AddRange(round);
+				}
+			}
+
+			return rounds;
+		}
+	}
+
+	class PartNode {
+		// Public Independant Properties
+		public Part part;
+		public List<PartNode> connectedNodes { get; private set; }
+
+
+		// Constructors
+		public PartNode(Part part, List<PartNode> connectedNodes) {
+			this.part = part;
+			this.connectedNodes = connectedNodes;
+		}
+
+		
+		// Methods
+		public bool SeverNode(PartNode node) {
+			if (connectedNodes.Contains(node)) {
+				// This node is attached to the other node.  Sever the connection.
+				connectedNodes.Remove(node);
+				node.SeverNode(this);
+				return true;
+			} else {
+				// This node is not attached to the other node.  Do nothing.
+				return false;
+			}
+		}
+
+		public bool AttachNode(PartNode node) {
+			if (!connectedNodes.Contains(node)) {
+				// This node is not attached to the other node.  Attach it.
+				connectedNodes.Add(this);
+				node.AttachNode(this);
+				return true;
+			} else {
+				// This node is already attached to the other node.  Do nothing.
+				return false;
+			}
+		}
+	}
+}
---
+++ Open Skies/Open Skies/Source/Part.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Skies.Source {
+	class Part {
+		// Public Independant Properties
+		public string name { get; private set; }
+		public double mass { get; protected set; }
+
+		// Constructors
+		public Part(string name, double mass) {
+			this.name = name;
+			this.mass = mass;
+		}
+	}
+
+	class Cockpit : Part {
+
+	}
+
+	class Wing : Part {
+
+	}
+}
